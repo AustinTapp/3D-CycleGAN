dfee57ec489ef5f12787d0f3319db6520f2b30e0
allielaur <108885447+allielaur@users.noreply.github.com>
2022-07-26 10:02:53 -0400

    Add files via upload

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -3,7 +3,8 @@ import itertools
 import random
 from .base_model import BaseModel
 from . import networks3D
-
+from MutualInformation import MI_pytorch
+from monai.losses import GlobalMutualInformationLoss
 
 class ImagePool():
     def __init__(self, pool_size):
@@ -66,7 +67,7 @@ class CycleGANModel(BaseModel):
         BaseModel.initialize(self, opt)
 
         # specify the training losses you want to print out. The program will call base_model.get_current_losses
-        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B']
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'idt_A', 'D_B', 'G_B', 'cycle_B', 'idt_B', 'G_A_MI', 'G_B_MI']
         # self.loss_names = ['D_A', 'G_A', 'cycle_A', 'cor_coe_GA', 'D_B', 'G_B', 'cycle_B', 'cor_coe_GB']
         # specify the images you want to save/display. The program will call base_model.get_current_visuals
         visual_names_A = ['real_A', 'fake_B', 'rec_A']
@@ -105,8 +106,9 @@ class CycleGANModel(BaseModel):
             self.fake_B_pool = ImagePool(opt.pool_size)
             # define loss functions
             self.criterionGAN = networks3D.GANLoss(use_lsgan=not opt.no_lsgan).to(self.device)
-            self.criterionCycle = torch.nn.L1Loss() # was L1
-            self.criterionIdt = torch.nn.L1Loss() # was L1
+            self.criterionCycle = torch.nn.MSELoss() # was L1
+            self.criterionIdt = torch.nn.MSELoss() # was L1
+            self.MICriterion = MI_pytorch(bins=50, min=-1, max=0, sigma=100, reduction='sum')
             # initialize optimizers
             self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), self.netG_B.parameters()),
                                                 lr=opt.lr, betas=(opt.beta1, 0.999))
@@ -121,6 +123,7 @@ class CycleGANModel(BaseModel):
         self.real_A = input[0 if AtoB else 1].to(self.device)
         self.real_B = input[1 if AtoB else 0].to(self.device)
         # self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        # self.mask_A = input[2].to(self.device) for mask
 
     def forward(self):
         self.fake_B = self.netG_A(self.real_A)
@@ -128,6 +131,10 @@ class CycleGANModel(BaseModel):
 
         self.fake_A = self.netG_B(self.real_B)
         self.rec_B = self.netG_A(self.fake_A)
+
+        """segmentation
+        t = Variable(torch.Tensor([100]))  # threshold
+        self.mask_B = (fake_A > t).float() # this is segmenting synthetic ct"""
 
     def backward_D_basic(self, netD, real, fake):
         # Real
@@ -188,14 +195,23 @@ class CycleGANModel(BaseModel):
         '''
         self.cor_coeLoss
         '''
-        self.loss_cor_coe_GA = networks3D.Cor_CoeLoss(self.fake_B,
+        """self.loss_cor_coe_GA = networks3D.Cor_CoeLoss(self.fake_B,
                                                       self.real_A) * lambda_co_A  # fake ct & real mr; Evaluate the Generator of ct(G_A)
         self.loss_cor_coe_GB = networks3D.Cor_CoeLoss(self.fake_A,
-                                                      self.real_B) * lambda_co_B  # fake mr & real ct; Evaluate the Generator of mr(G_B)
+                                                      self.real_B) * lambda_co_B  # fake mr & real ct; Evaluate the Generator of mr(G_B)"""
+
+
+        self.loss_G_A_MI = self.MICriterion(self.fake_B, self.real_A) * lambda_co_A
+        self.loss_G_B_MI = self.MICriterion(self.fake_A, self.real_B) * lambda_co_B
+
+        # self.loss_seg = self.CriterionCycle(self.mask_B, self.mask_A) # for mask
+
+        # need to add the MI loss/ shape constrain loss to the combined loss
 
         # combined loss
-        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B # original
         # self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B + self.loss_cor_coe_GA + self.loss_cor_coe_GB
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B + self.loss_G_A_MI + self.loss_G_B_MI # mutual information added
 
         self.loss_G.backward()
 
